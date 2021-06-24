7a5a5fd25fb3eab0fc74f8e95bab5cec6a4730af
Mujeeb1408 <83758082+Mujeeb1408@users.noreply.github.com>
2021-06-24 19:51:00 +0530

    Delete deci_binary.py

--- deci_binary.py
+++
@@ -1,9 +0,0 @@
-# program to convert the decimal number to binary number
-def decimal_binary(a):
-    if a >= 1:
-        decimal_binary(a // 2)
-    print(a % 2, end=' ')
-
-
-x = int(input('Enter a decimal number : '))
-decimal_binary(x)
